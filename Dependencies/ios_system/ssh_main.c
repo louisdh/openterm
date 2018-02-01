a863a826557acc16595df19dd7eec818c94b757a
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2018-02-01 19:01:41 +0100

    Trying to fix compilation

--- Dependencies/ios_system/ssh_main.c
+++ Dependencies/ios_system/ssh_main.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#include "libssh2/libssh2.h"
+#include "libssh2.h"
 #include <sys/fcntl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
