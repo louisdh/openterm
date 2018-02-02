89da3ba4b060fa81428295b5aefc93e0b439e901
Max Katzmann <m.katzmann@me.com>
2018-02-02 16:21:23 +0100

    The directory to store the bookmarks is now obtained using the DocumentManager.

--- OpenTerm/Util/Bookmarks/BookmarkManager.swift
+++ OpenTerm/Util/Bookmarks/BookmarkManager.swift
@@ -29,84 +29,80 @@ class BookmarkManager {
         /**
          *  Get the document directory of the app.
          */
-        if let dir = FileManager.default.urls(for: .documentDirectory,
-                                              in: .userDomainMask).first {
+        let dir = DocumentManager.shared.activeDocumentsFolderURL
             
-            /**
-             *  Get the directory where the bookmarks are saved.
-             */
-            let bookmarkDirectoryURL = dir.appendingPathComponent(BookmarkManager.bookmarkDirectory,
-                                                                  isDirectory: true)
-            
-            /**
-             *  Create the bookmarks directory (if it doesn't exist)
-             */
-            do {
-                try FileManager.default.createDirectory(at: bookmarkDirectoryURL,
-                                                        withIntermediateDirectories: true,
-                                                        attributes: nil)
-                
-                /**
-                 *  Get all files that are in the bookmarks directory.
-                 */
-                let bookmarkFiles = try FileManager.default.contentsOfDirectory(atPath: bookmarkDirectoryURL.path)
-                
-                /**
-                 *  The array that will hold the obtained URLs. This will be returned eventually.
-                 */
-                var bookmarkURLs = [URL]()
-                
-                /**
-                 *  Iterate all bookmark filenames.
-                 */
-                for bookmarkFileName in bookmarkFiles {
-                    
-                    /**
-                     *  Get the url of the current bookmark file.
-                     */
-                    let bookmarkDataURL = bookmarkDirectoryURL.appendingPathComponent(bookmarkFileName)
-                    
-                    /**
-                     *  We try to load the bookmark from the file.
-                     */
-                    let loadedBookmark = try URL.bookmarkData(withContentsOf: bookmarkDataURL)
-                    
-                    /**
-                     *  This variable will indicate whether the bookmark is stale.
-                     *  If the bookmark is stale we create a new bookmark with the
-                     *  obtained URL and save the new one instead of the old one.
-                     */
-                    var isStale = true
-                    
-                    /**
-                     *  Try to obtain the URL from the bookmark.
-                     */
-                    if let loadedBookmarkURL = try URL(resolvingBookmarkData: loadedBookmark, bookmarkDataIsStale: &isStale) {
-                        
-                        /**
-                         *  If the bookmark is stale, we create a new bookmark
-                         *  from the obtained URL.
-                         */
-                        if isStale {
-                            do {
-                                try self.saveBookmarkURL(url: loadedBookmarkURL)
-                            }
-                        }
-                        
-                        /**
-                         *  Append the loaded URLs.
-                         */
-                        bookmarkURLs.append(loadedBookmarkURL)
-                    }
-                }
-                
-                return bookmarkURLs
-            } catch {
-                return []
-            }
-        }
+        /**
+         *  Get the directory where the bookmarks are saved.
+         */
+        let bookmarkDirectoryURL = dir.appendingPathComponent(BookmarkManager.bookmarkDirectory,
+                                                              isDirectory: true)
         
-        return []
+        /**
+         *  Create the bookmarks directory (if it doesn't exist)
+         */
+        do {
+            try FileManager.default.createDirectory(at: bookmarkDirectoryURL,
+                                                    withIntermediateDirectories: true,
+                                                    attributes: nil)
+            
+            /**
+             *  Get all files that are in the bookmarks directory.
+             */
+            let bookmarkFiles = try FileManager.default.contentsOfDirectory(atPath: bookmarkDirectoryURL.path)
+            
+            /**
+             *  The array that will hold the obtained URLs. This will be returned eventually.
+             */
+            var bookmarkURLs = [URL]()
+            
+            /**
+             *  Iterate all bookmark filenames.
+             */
+            for bookmarkFileName in bookmarkFiles {
+                
+                /**
+                 *  Get the url of the current bookmark file.
+                 */
+                let bookmarkDataURL = bookmarkDirectoryURL.appendingPathComponent(bookmarkFileName)
+                
+                /**
+                 *  We try to load the bookmark from the file.
+                 */
+                let loadedBookmark = try URL.bookmarkData(withContentsOf: bookmarkDataURL)
+                
+                /**
+                 *  This variable will indicate whether the bookmark is stale.
+                 *  If the bookmark is stale we create a new bookmark with the
+                 *  obtained URL and save the new one instead of the old one.
+                 */
+                var isStale = true
+                
+                /**
+                 *  Try to obtain the URL from the bookmark.
+                 */
+                if let loadedBookmarkURL = try URL(resolvingBookmarkData: loadedBookmark, bookmarkDataIsStale: &isStale) {
+                    
+                    /**
+                     *  If the bookmark is stale, we create a new bookmark
+                     *  from the obtained URL.
+                     */
+                    if isStale {
+                        do {
+                            try self.saveBookmarkURL(url: loadedBookmarkURL)
+                        }
+                    }
+                    
+                    /**
+                     *  Append the loaded URLs.
+                     */
+                    bookmarkURLs.append(loadedBookmarkURL)
+                }
+            }
+            
+            return bookmarkURLs
+        } catch {
+            return []
+        }
     }
     
     /// Determines the current directory and saves the corresponding path as
@@ -168,38 +164,36 @@ class BookmarkManager {
              *  Get the document directory of the app. The bookmarks will be saved
              *  in a hidden folder there.
              */
-            if let dir = FileManager.default.urls(for: .documentDirectory,
-                                                  in: .userDomainMask).first {
+            let dir = DocumentManager.shared.activeDocumentsFolderURL
                 
-                /**
-                 *  Get the URL of the bookmark directory (where the bookmarks are saved).
-                 */
-                let bookmarkDirectoryURL = dir.appendingPathComponent(BookmarkManager.bookmarkDirectory,
-                                                                      isDirectory: true)
-                
-                /**
-                 *  Create the bookmarks directory (if it doesn't exist)
-                 */
-                try FileManager.default.createDirectory(at: bookmarkDirectoryURL,
-                                                        withIntermediateDirectories: true,
-                                                        attributes: nil)
-                
-                /**
-                 *  The URL for where the bookmark data will be saved.
-                 */
-                let bookmarkDataURL = bookmarkDirectoryURL.appendingPathComponent(url.lastPathComponent,
-                                                                                  isDirectory: false)
-                
-                /**
-                 *  Actually saving the bookmark data.
-                 */
-                try URL.writeBookmarkData(bookmark, to: bookmarkDataURL)
-                
-                /**
-                 *  Tell the bookmark view controller that the bookmarks were updated.
-                 */
-                self.delegate?.bookmarksWereUpdated()
-            }
+            /**
+             *  Get the URL of the bookmark directory (where the bookmarks are saved).
+             */
+            let bookmarkDirectoryURL = dir.appendingPathComponent(BookmarkManager.bookmarkDirectory,
+                                                                  isDirectory: true)
+            
+            /**
+             *  Create the bookmarks directory (if it doesn't exist)
+             */
+            try FileManager.default.createDirectory(at: bookmarkDirectoryURL,
+                                                    withIntermediateDirectories: true,
+                                                    attributes: nil)
+            
+            /**
+             *  The URL for where the bookmark data will be saved.
+             */
+            let bookmarkDataURL = bookmarkDirectoryURL.appendingPathComponent(url.lastPathComponent,
+                                                                              isDirectory: false)
+            
+            /**
+             *  Actually saving the bookmark data.
+             */
+            try URL.writeBookmarkData(bookmark, to: bookmarkDataURL)
+            
+            /**
+             *  Tell the bookmark view controller that the bookmarks were updated.
+             */
+            self.delegate?.bookmarksWereUpdated()
         } catch {
             throw error
         }
@@ -212,36 +206,34 @@ class BookmarkManager {
         /**
          *  Get the document directory of the app.
          */
-        if let dir = FileManager.default.urls(for: .documentDirectory,
-                                              in: .userDomainMask).first {
+        let dir = DocumentManager.shared.activeDocumentsFolderURL
+        
+        /**
+         *  Get the directory of where the bookmarks are saved.
+         */
+        let bookmarkDirectoryURL = dir.appendingPathComponent(BookmarkManager.bookmarkDirectory,
+                                                              isDirectory: true)
+        
+        /**
+         *  Create the bookmarks directory (if it doesn't exist)
+         */
+        do {
+            try FileManager.default.createDirectory(at: bookmarkDirectoryURL,
+                                                    withIntermediateDirectories: true,
+                                                    attributes: nil)
             
             /**
-             *  Get the directory of where the bookmarks are saved.
+             *  The URL for where the bookmark data was saved to.
              */
-            let bookmarkDirectoryURL = dir.appendingPathComponent(BookmarkManager.bookmarkDirectory,
-                                                                  isDirectory: true)
+            let bookmarkFileURL = bookmarkDirectoryURL.appendingPathComponent(bookmarkURL.lastPathComponent,
+                                                                              isDirectory: false)
             
             /**
-             *  Create the bookmarks directory (if it doesn't exist)
+             *  Actually deleting the file.
              */
-            do {
-                try FileManager.default.createDirectory(at: bookmarkDirectoryURL,
-                                                        withIntermediateDirectories: true,
-                                                        attributes: nil)
-                
-                /**
-                 *  The URL for where the bookmark data was saved to.
-                 */
-                let bookmarkFileURL = bookmarkDirectoryURL.appendingPathComponent(bookmarkURL.lastPathComponent,
-                                                                                  isDirectory: false)
-                
-                /**
-                 *  Actually deleting the file.
-                 */
-                try FileManager.default.removeItem(at: bookmarkFileURL)
-            } catch {
-                
-            }
+            try FileManager.default.removeItem(at: bookmarkFileURL)
+        } catch {
+            
         }
     }
     
