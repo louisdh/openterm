81c39761135056110f71545d117a079d98e4f40a
Louis D'hauwe <louisdhauwe@silverfox.be>
2018-03-27 21:14:25 +0200

    Fix typo

--- OpenTerm/Controller/TerminalViewController.swift
+++ OpenTerm/Controller/TerminalViewController.swift
@@ -617,7 +617,7 @@ private extension TerminalViewController {
 				return
 			}
 
-			// Dismiss ourself, and run action with presentiing view
+			// Dismiss ourself, and run action with presenting view
 			let item = items[indexPath.row]
 			presentingViewController?.dismiss(animated: true, completion: {
 				item.action(presentingView)
