3f826aaf5725f80d6a198f5e823419896e47864f
Louis D'hauwe <louisdhauwe@silverfox.be>
2018-04-01 19:33:17 +0200

    Improve UIDocumentPickerViewController error handling

--- OpenTerm/Controller/TerminalViewController.swift
+++ OpenTerm/Controller/TerminalViewController.swift
@@ -378,8 +378,14 @@ extension TerminalViewController: UIDocumentPickerDelegate {
 			return
 		}
 
-		_ = firstFolder.startAccessingSecurityScopedResource()
-
+		let success = firstFolder.startAccessingSecurityScopedResource()
+		let isReadable = DocumentManager.shared.fileManager.isReadableFile(atPath: firstFolder.path)
+		
+		guard success && isReadable else {
+			showAlert("Error", message: "Could not access folder.")
+			return
+		}
+		
 		self.terminalView.executor.currentWorkingDirectory = firstFolder
 		self.terminalView.executor.setLocalMiniRoot()
 	}
