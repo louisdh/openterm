003a069a127d752a68d34875769e86820394996c
Louis D'hauwe <louisdhauwe@silverfox.be>
2018-05-11 21:55:02 +0200

    Improve documentation viewer.

--- OpenTerm/Controller/Scripting/Documentation/CubDocumentationItemViewController.swift
+++ OpenTerm/Controller/Scripting/Documentation/CubDocumentationItemViewController.swift
@@ -19,28 +19,49 @@ class CubDocumentationItemViewController: UIViewController {
         super.viewDidLoad()
 
 		self.title = item.title
-		textView.attributedText = attributedString(item: item)
-		textView.isEditable = false
+		self.textView.attributedText = attributedString(item: item)
+		self.textView.isEditable = false
 		
 		self.navigationController?.navigationBar.barStyle = .blackTranslucent
 		self.view.backgroundColor = .panelBackgroundColor
 		self.textView.backgroundColor = .panelBackgroundColor
 		self.textView.textColor = .white
+		self.textView.textContainerInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+		self.textView.indicatorStyle = .white
 		
     }
 	
 	func attributedString(item: DocumentationItem) -> NSAttributedString {
 		
 		let font = UIFont.systemFont(ofSize: 17.0)
+		let boldFont = UIFont.boldSystemFont(ofSize: 17.0)
+
+		let titleFont = UIFont.systemFont(ofSize: 22, weight: .medium)
+
+		let paragraphStyle = NSMutableParagraphStyle()
+		paragraphStyle.paragraphSpacingBefore = 16
+		paragraphStyle.paragraphSpacing = 8
+
+		let titleAttributes: [NSAttributedStringKey: Any] = [.foregroundColor: UIColor.white,
+															 .font: titleFont,
+															 .paragraphStyle: paragraphStyle]
 		
 		let attributedString = NSMutableAttributedString(string: "", attributes: [.foregroundColor: UIColor.white, .font: font])
 		
-		let definitionAttrString = NSAttributedString(string: item.definition + "\n\n", attributes: [.font: UIFont(name: "Menlo-Regular", size: 17.0)!])
+		let definitionAttrString = NSAttributedString(string: item.definition + "\n", attributes: [.font: UIFont(name: "Menlo-Regular", size: 17.0)!])
 		
+		let declarationTitle = NSMutableAttributedString(string: "Declaration\n", attributes: titleAttributes)
+		
+		attributedString.append(declarationTitle)
+
 		attributedString.append(definitionAttrString)
 		
 		switch item.type {
 		case .function(let functionDocumentation):
+			
+			let overviewTitle = NSMutableAttributedString(string: "Overview\n", attributes: titleAttributes)
+			
+			attributedString.append(overviewTitle)
 			
 			let descr = functionDocumentation.description ?? "No description"
 			
@@ -48,25 +69,45 @@ class CubDocumentationItemViewController: UIViewController {
 			
 			attributedString.append(descrAttrString)
 			
-			for arg in functionDocumentation.arguments {
+			if !functionDocumentation.arguments.isEmpty {
 				
-				let argDescr: String
+				let argumentsTitle = NSMutableAttributedString(string: "Parameters\n", attributes: titleAttributes)
 				
-				if let argDescription = functionDocumentation.argumentDescriptions[arg] {
-					argDescr = "\(arg): " + argDescription
-				} else {
-					argDescr = "\(arg): No description"
+				attributedString.append(argumentsTitle)
+				
+				for arg in functionDocumentation.arguments {
+					
+					let argNameParagraphStyle = NSMutableParagraphStyle()
+					argNameParagraphStyle.headIndent = 8
+					argNameParagraphStyle.firstLineHeadIndent = 8
+					
+					let argNameAttrString = NSAttributedString(string: "\(arg)\n", attributes: [.font: boldFont,
+																								  .paragraphStyle: argNameParagraphStyle])
+					
+					attributedString.append(argNameAttrString)
+					
+					let argDescrParagraphStyle = NSMutableParagraphStyle()
+					argDescrParagraphStyle.headIndent = 16
+					argDescrParagraphStyle.firstLineHeadIndent = 16
+					
+					let argDescr = (functionDocumentation.argumentDescriptions[arg] ?? "No description") + "\n"
+
+					let argDescrAttrString = NSAttributedString(string: argDescr + "\n", attributes: [.font: font,
+																									  .paragraphStyle: argDescrParagraphStyle])
+					
+					attributedString.append(argDescrAttrString)
+					
 				}
 				
-				let argDescrAttrString = NSAttributedString(string: argDescr + "\n", attributes: [.font: font])
-				
-				attributedString.append(argDescrAttrString)
-				
 			}
-			
-			if let returnDescription = functionDocumentation.returnDescription {
+
+			if functionDocumentation.returns {
 				
-				let returnsDescr = "Returns: " + returnDescription
+				let returnValueTitle = NSMutableAttributedString(string: "Return value\n", attributes: titleAttributes)
+				
+				attributedString.append(returnValueTitle)
+				
+				let returnsDescr = functionDocumentation.returnDescription ?? "No description"
 				
 				let returnsDescrAttrString = NSAttributedString(string: returnsDescr + "\n", attributes: [.font: font])
 				
@@ -76,6 +117,10 @@ class CubDocumentationItemViewController: UIViewController {
 			
 		case .variable(let variableDocumentation):
 			
+			let overviewTitle = NSMutableAttributedString(string: "Overview\n", attributes: titleAttributes)
+			
+			attributedString.append(overviewTitle)
+			
 			let descr = variableDocumentation.description ?? "No description"
 			
 			let descrAttrString = NSAttributedString(string: descr + "\n", attributes: [.font: font])
@@ -84,25 +129,41 @@ class CubDocumentationItemViewController: UIViewController {
 			
 		case .struct(let structDocumentation):
 			
+			let overviewTitle = NSMutableAttributedString(string: "Overview\n", attributes: titleAttributes)
+			
+			attributedString.append(overviewTitle)
+			
 			let descr = structDocumentation.description ?? "No description"
 			
 			let descrAttrString = NSAttributedString(string: descr + "\n", attributes: [.font: font])
 			
 			attributedString.append(descrAttrString)
 			
+			let argumentsTitle = NSMutableAttributedString(string: "Parameters\n", attributes: titleAttributes)
+			
+			attributedString.append(argumentsTitle)
+			
 			for member in structDocumentation.members {
 				
-				let memberDescr: String
+				let argNameParagraphStyle = NSMutableParagraphStyle()
+				argNameParagraphStyle.headIndent = 8
+				argNameParagraphStyle.firstLineHeadIndent = 8
 				
-				if let memberDescription = structDocumentation.memberDescriptions[member] {
-					memberDescr = "\(member): " + (memberDescription ?? "No description")
-				} else {
-					memberDescr = "\(member): No description"
-				}
+				let argNameAttrString = NSAttributedString(string: "\(member)\n", attributes: [.font: boldFont,
+																							.paragraphStyle: argNameParagraphStyle])
 				
-				let memberDescrAttrString = NSAttributedString(string: memberDescr + "\n", attributes: [.font: font])
+				attributedString.append(argNameAttrString)
 				
-				attributedString.append(memberDescrAttrString)
+				let argDescrParagraphStyle = NSMutableParagraphStyle()
+				argDescrParagraphStyle.headIndent = 16
+				argDescrParagraphStyle.firstLineHeadIndent = 16
+				
+				let argDescr = ((structDocumentation.memberDescriptions[member] ?? "No description") ?? "No description") + "\n"
+				
+				let argDescrAttrString = NSAttributedString(string: argDescr + "\n", attributes: [.font: font,
+																								  .paragraphStyle: argDescrParagraphStyle])
+				
+				attributedString.append(argDescrAttrString)
 				
 			}
 		}
